2e699fcf751afc52c9d7f0002e31b259c2214590
Anna Godlewska <godlewska.anna04@gmail.com>
2026-06-07 20:36:20 +0200

    feat: dodanie kodów QR przez zewnętrzne API solsigs

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*+$=)6#a4+bj3q5f@ixl05fz-0#7)ne2g)r*m#plp53tm9j^xr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
